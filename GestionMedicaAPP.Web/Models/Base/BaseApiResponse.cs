8ea7a404309640b6ee745b7c475fccf2b3a72914
Brian Alexander Tavarez Duarte <briam1952@gmail.com>
2025-05-17 00:36:57 -0400

    Mejoras en la gestión de respuestas API y logging
    Se ha añadido un constructor a `BaseApiResponse` para inicializar `isSuccess`.
    En `AppointmentServiceApi`, se ha incorporado un logger en el constructor.
    Se han optimizado los métodos `GetAllAsync`, `GetByIdAsync`, `CreateAsync`, `UpdateAsync` y `DeleteAsync` para manejar respuestas HTTP de manera más robusta, utilizando `EnsureSuccessStatusCode()` y `ReadFromJsonAsync` en lugar de `JsonConvert`.
    Además, se han añadido mensajes de éxito y error a los modelos de respuesta y se ha implementado el registro de errores.

--- GestionMedicaAPP.Web/Models/Base/BaseApiResponse.cs
+++ GestionMedicaAPP.Web/Models/Base/BaseApiResponse.cs
@@ -2,6 +2,10 @@
 {
     public class BaseApiResponse
     {
+        public BaseApiResponse()
+        {
+            isSuccess = true;
+        }
         public bool isSuccess { get; set; }
         public string message { get; set; }
     }
--- GestionMedicaAPP.Web/Service/ServiceApi/Appointmets/AppointmentServiceApi.cs
+++ GestionMedicaAPP.Web/Service/ServiceApi/Appointmets/AppointmentServiceApi.cs
@@ -2,73 +2,109 @@
 using GestionMedicaAPP.Web.Models.Base;
 using GestionMedicaAPP.Web.Models.ControllersModels.appointments.appoitments;
 using GestionMedicaAPP.Web.Service.Interfaces.Appointmets;
-using Newtonsoft.Json;
 
 namespace GestionMedicaAPP.Web.Service.ServiceApi.Appointmets
 {
     public class AppointmentServiceApi : IAppointmentsServiceApi
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<AppointmentServiceApi> _logger;
 
-        public AppointmentServiceApi(HttpClient httpClient, IConfiguration configuration)
+        public AppointmentServiceApi(HttpClient httpClient,ILogger<AppointmentServiceApi> logger, IConfiguration configuration)
         {
+            _logger = logger;
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(configuration["ApiConecction:ApiBaseAppointments"]);
         }
 
         public async Task<AppointmentsGetAllModel> GetAllAsync()
         {
-            var response = await _httpClient.GetAsync("Appointments/GetAppointments");
-            if (response.IsSuccessStatusCode)
+            AppointmentsGetAllModel appointmentsGetAllModel = new AppointmentsGetAllModel();
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<AppointmentsGetAllModel>(responseString);
+                var response = await _httpClient.GetAsync("Appointments/GetAppointments");
+
+                response.EnsureSuccessStatusCode();
+                appointmentsGetAllModel.message = "Citas encontradas";
+                appointmentsGetAllModel = await response.Content.ReadFromJsonAsync<AppointmentsGetAllModel>();
             }
-            return null;
+            catch (Exception ex)
+            {
+                appointmentsGetAllModel.isSuccess = false;
+                appointmentsGetAllModel.message = "Error obteniendo las citas";
+                _logger.LogError($"{appointmentsGetAllModel.message} {ex.ToString()}");
+            }
+            return appointmentsGetAllModel;
         }
 
         public async Task<AppointmentsGetByIdModel> GetByIdAsync(int id)
         {
-            var response = await _httpClient.GetAsync($"Appointments/GetAppointmentsById?id={id}");
-            if (response.IsSuccessStatusCode)
+            AppointmentsGetByIdModel appointmentsGetByIdModel = new AppointmentsGetByIdModel();
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<AppointmentsGetByIdModel>(responseString);
+                var response = await _httpClient.GetAsync($"Appointments/GetAppointmentsById?id={id}");
+                response.EnsureSuccessStatusCode();
+                appointmentsGetByIdModel.message = "Cita encontrada";
+                appointmentsGetByIdModel = await response.Content.ReadFromJsonAsync<AppointmentsGetByIdModel>();
             }
-            return null;
+            catch (Exception ex)
+            {
+                appointmentsGetByIdModel.isSuccess = false;
+                appointmentsGetByIdModel.message = "Error obteniendo la cita";
+                _logger.LogError($"{appointmentsGetByIdModel.message} {ex.ToString()}");
+            }
+            return appointmentsGetByIdModel;
         }
 
         public async Task<BaseApiResponse> CreateAsync(AppointmentsSaveDto appointment)
         {
-            var response = await _httpClient.PostAsJsonAsync("Appointments/SaveAppointments", appointment);
-            if (response.IsSuccessStatusCode)
+            BaseApiResponse apiResponse = new BaseApiResponse();
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<BaseApiResponse>(responseString);
+                var response = await _httpClient.PostAsJsonAsync("Appointments/SaveAppointments", appointment);
+                response.EnsureSuccessStatusCode();
+                apiResponse.message = "Cita guardada";
             }
-            return null;
+            catch (Exception ex)
+            {
+                apiResponse.message = "Error al guardar cita";
+                _logger.LogError($"{apiResponse.message} {ex.ToString()}");
+            }
+            return apiResponse;
         }
 
         public async Task<BaseApiResponse> UpdateAsync(AppointmentsUpdateDto appointment)
         {
-            var response = await _httpClient.PutAsJsonAsync($"Appointments/UpdateAppointments", appointment);
-            if (response.IsSuccessStatusCode)
+            BaseApiResponse apiResponse = new BaseApiResponse();
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<BaseApiResponse>(responseString);
+                var response = await _httpClient.PutAsJsonAsync($"Appointments/UpdateAppointments", appointment);
+                response.EnsureSuccessStatusCode();
+                apiResponse.message = "Cita actualizada correctamente";
             }
-            return null;
+            catch (Exception ex)
+            {
+                apiResponse.message = "Error al actualizar cita";
+                _logger.LogError($"{apiResponse.message} {ex.ToString()}");
+            }
+            return apiResponse;
         }
 
         public async Task<BaseApiResponse> DeleteAsync(int id)
         {
-            var response = await _httpClient.DeleteAsync($"Appointments/RemoveAppointments?id={id}");
-            if (response.IsSuccessStatusCode)
+            BaseApiResponse apiResponse = new BaseApiResponse();
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<BaseApiResponse>(responseString);
+                var response = await _httpClient.DeleteAsync($"Appointments/RemoveAppointments?id={id}");
+                response.EnsureSuccessStatusCode();
+                apiResponse.message = "Cita eliminada correctamente";
             }
-            return null;
+            catch (Exception ex)
+            {
+                apiResponse.message = "Error al eliminar cita";
+                _logger.LogError($"{apiResponse.message} {ex.ToString()}");
+            }
+            return apiResponse;
         }
     }
 }
